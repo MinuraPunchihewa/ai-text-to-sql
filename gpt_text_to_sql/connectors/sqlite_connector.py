9d9d3a4146773e64cd35f0675de987c60cfe5a48
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-02 20:20:09 +0530

    implemented the SQLite Connector

---
+++ gpt_text_to_sql/connectors/sqlite_connector.py
@@ -0,0 +1,17 @@
+from typing import Optional, Dict, Text
+from sqlalchemy import create_engine, inspect
+from .database_connector import DatabaseConnector
+
+
+class SqliteConnector(DatabaseConnector):
+    def __init__(self, name: Text, connection_data: Optional[Dict]):
+        super().__init__(name, connection_data)
+
+        engine = create_engine(f'sqlite:///{self.connection_data["db_file"]}')
+        self.inspector = inspect(engine)
+
+    def get_tables(self):
+        return self.inspector.get_table_names()
+
+    def get_columns(self, table_name):
+        return self.inspector.get_columns(table_name)
