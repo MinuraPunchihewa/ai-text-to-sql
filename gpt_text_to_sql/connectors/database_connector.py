17bebc27523606a37adde3cb6d492a38238d20d3
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-02 20:10:53 +0530

    implemented the DatabaseConnector interface

--- gpt_text_to_sql/connectors/database_connector.py
+++ gpt_text_to_sql/connectors/database_connector.py
@@ -1,5 +1,16 @@
+from typing import Optional
 from abc import ABC, abstractmethod
 
 
 class DatabaseConnector(ABC):
-    pass
+    def __int__(self, name: str, connection_data: Optional[dict]):
+        self.name = name
+        self.connection_data = connection_data
+
+    @abstractmethod
+    def get_tables(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_columns(self, table_name):
+        raise NotImplementedError
