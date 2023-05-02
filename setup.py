8155a0d891f325970ce6f508e5906fff1a993f73
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-02 21:00:31 +0530

    updated the Python versio required to >=3.9

--- setup.py
+++ setup.py
@@ -30,5 +30,5 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ),
-    python_requires=">=3.7"
+    python_requires=">=3.9"
 )
