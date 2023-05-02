eac7cb847fdfefb5f82eed4d821f3b0d6bf4a3ea
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-03 00:51:27 +0530

    implemented the query() method in the SQLite Connector

--- gpt_text_to_sql/connectors/sqlite_connector.py
+++ gpt_text_to_sql/connectors/sqlite_connector.py
@@ -1,6 +1,6 @@
 from typing import Optional, Dict, Text
-from sqlalchemy import create_engine, inspect
 from .database_connector import DatabaseConnector
+from sqlalchemy import create_engine, inspect, text
 
 
 class SQLiteConnector(DatabaseConnector):
@@ -9,11 +9,14 @@ class SQLiteConnector(DatabaseConnector):
     def __init__(self, name: Text, connection_data: Optional[Dict]):
         super().__init__(name, connection_data)
 
-        engine = create_engine(f'sqlite:///{self.connection_data["db_file"]}')
-        self.inspector = inspect(engine)
+        self.engine = create_engine(f'sqlite:///{self.connection_data["db_file"]}')
+        self.inspector = inspect(self.engine)
 
     def get_tables(self):
         return self.inspector.get_table_names()
 
     def get_columns(self, table_name):
         return self.inspector.get_columns(table_name)
+
+    def query(self, query: Text):
+        return self.engine.execute(text(query))
