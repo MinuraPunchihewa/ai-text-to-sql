305eebd879f48ee225a8735281d5636df7903b3e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-04 18:03:46 +0530

    refactored DatabaseConnector with implementations of common methods to make the code cleaner and more modular

--- gpt_text_to_sql/connectors/database_connector.py
+++ gpt_text_to_sql/connectors/database_connector.py
@@ -1,19 +1,29 @@
-from typing import Optional, Text, Dict
 from abc import ABC, abstractmethod
+from typing import Optional, Text, Dict
+from sqlalchemy import inspect, text
 
 
 class DatabaseConnector(ABC):
     def __init__(self, name: Text, connection_data: Optional[Dict]):
         self.name = name
         self.connection_data = connection_data
+        self.connection = self.create_connection()
+        self.inspector = self.create_inspector()
 
     @abstractmethod
+    def create_connection(self):
+        raise NotImplementedError
+
+    def create_inspector(self):
+        return inspect(self.connection)
+
     def get_tables(self):
-        raise NotImplementedError
+        return self.inspector.get_table_names()
 
-    @abstractmethod
-    def get_columns(self, table_name: Text):
-        raise NotImplementedError
+    def get_columns(self, table_name):
+        return [column['name'] for column in self.inspector.get_columns(table_name)]
 
     def query(self, query: Text):
-        raise NotImplementedError
+        with self.connection.connect() as conn:
+            result = conn.execute(text(query))
+        return result.fetchall()
