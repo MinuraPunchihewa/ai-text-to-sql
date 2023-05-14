bd8aa18bdbb1e037eab66b49892f7ab69b573a75
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-14 19:39:47 +0530

    added docstrings and type hints to the TextToSQL class

--- gpt_text_to_sql/text_to_sql.py
+++ gpt_text_to_sql/text_to_sql.py
@@ -2,7 +2,7 @@ import os
 import openai
 import pandas as pd
 from .gpt import GPT
-from typing import Optional, Text, Dict
+from typing import Optional, Text, List, Dict
 
 import logging
 import logging.config
@@ -14,26 +14,56 @@ logger = logging.getLogger()
 
 
 class TextToSQL:
+    """
+    The class for converting text to SQL query and querying the database.
+
+    Parameters:
+    -----------
+    connector_name: Text
+        The name of the connector.
+    connection_data: Dict
+        A dictionary containing the configuration parameters for the database connection.
+    """
     def __init__(self, connector_name: Text, connection_data: Optional[Dict], api_key: Optional[Text] = None):
         self.gpt = GPT(connector_name, connection_data)
         self._set_openai_api_key(api_key)
 
         self.logger = logger
 
-    def convert_text_to_sql(self, text: Text):
+    def convert_text_to_sql(self, text: Text) -> Text:
+        """
+        Convert text to SQL query.
+        :param text: The Text to convert to SQL query.
+        :return: The converted SQL query.
+        """
         sql = self.gpt.get_top_reply(text).strip()
         self.logger.info(f"SQL query: {sql}")
         return sql
 
-    def query(self, query: Text):
-        sql = self.convert_text_to_sql(query)
+    def query(self, text: Text) -> List[Dict]:
+        """
+        Query the database.
+        :param text: The text to convert to SQL query and query the database.
+        :return: The query result.
+        """
+        sql = self.convert_text_to_sql(text)
         return self.gpt.connector.query(sql)
 
-    def query_df(self, query: Text):
-        sql = self.convert_text_to_sql(query)
+    def query_df(self, text: Text) -> pd.DataFrame:
+        """
+        Query the database and return the result as a Pandas DataFrame.
+        :param text: The text to convert to SQL query and query the database.
+        :return: A Pandas DataFrame containing the query result.
+        """
+        sql = self.convert_text_to_sql(text)
         return pd.DataFrame(self.gpt.connector.query(sql))
 
     def _set_openai_api_key(self, api_key):
+        """
+        Set the OpenAI API key.
+        :param api_key: A valid OpenAI API key.
+        :return: None.
+        """
         api_key = api_key or os.getenv('OPENAI_API_KEY')
         if api_key is not None:
             openai.api_key = api_key
