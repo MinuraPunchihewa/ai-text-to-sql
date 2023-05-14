9af64d3145d2e3cdd8718581f5ac36c5ea35f093
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-14 19:49:30 +0530

    added docstrings and type hints to the DatabaseConnectorFactory class

--- gpt_text_to_sql/connectors/database_connector_factory.py
+++ gpt_text_to_sql/connectors/database_connector_factory.py
@@ -4,6 +4,15 @@ from .database_connector import DatabaseConnector
 
 
 class DatabaseConnectorFactory:
+    """
+    The class for building database connectors.
+    """
     @staticmethod
     def build_connector(name: str, connection_data: Optional[Dict]) -> DatabaseConnector:
+        """
+        Build a database connector.
+        :param name: The name of the connector.
+        :param connection_data: A dictionary containing the configuration parameters for the database connection.
+        :return: A database connector.
+        """
         return globals()[f"{name}Connector"](name, connection_data)
