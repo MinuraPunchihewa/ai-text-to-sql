ac734cd40c0e23e9f7d02c6e54520da8bfb21822
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-17 00:34:37 +0530

    added a couple more unit tests for the SQLite Connector

--- tests/test_sqlite_connector.py
+++ tests/test_sqlite_connector.py
@@ -23,3 +23,19 @@ class TestSQLiteConnector(unittest.TestCase):
             self.tts.convert_text_to_sql("Get me a list of distinct genres."),
             "SELECT DISTINCT Name FROM genres"
         )
+
+    def test_advanced_convert_text_to_sql(self):
+        self.assertEqual(
+            self.tts.convert_text_to_sql("Get me the names of 5 Rock songs."),
+            "SELECT Name FROM tracks\n"
+            "INNER JOIN genres ON genres.GenreId = tracks.GenreId\n"
+            "WHERE genres.Name = 'Rock'\n"
+            "LIMIT 5"
+        )
+
+    def test_query(self):
+        genres = self.tts.query("Get me a list of 5 distinct genres.")
+        self.assertEqual(
+            [genre[0] for genre in genres],
+            ['Rock', 'Jazz', 'Metal', 'Alternative & Punk', 'Rock And Roll']
+        )
