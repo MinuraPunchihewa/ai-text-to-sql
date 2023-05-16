55bd998f4bfbd1ae631e08d1a626e4644b8a8552
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-17 00:06:56 +0530

    improved the approach used by the Database Connector Factory to build Connectors

--- gpt_text_to_sql/connectors/database_connector_factory.py
+++ gpt_text_to_sql/connectors/database_connector_factory.py
@@ -1,4 +1,6 @@
-from typing import Optional, Dict
+import sys
+import inspect
+from typing import Optional, Dict, Type
 
 from gpt_text_to_sql.connectors import *
 from .database_connector import DatabaseConnector
@@ -16,4 +18,20 @@ class DatabaseConnectorFactory:
         :param connection_data: A dictionary containing the configuration parameters for the database connection.
         :return: A database connector.
         """
-        return globals()[f"{name}Connector"](name, connection_data)
+        connectors = DatabaseConnectorFactory._discover_connectors()
+        if name in connectors:
+            return connectors[name](name, connection_data)
+        else:
+            raise ValueError(f"Unsupported connector: {name}")
+
+    @staticmethod
+    def _discover_connectors() -> Dict[str, Type[DatabaseConnector]]:
+        """
+        Discover available connectors dynamically.
+        :return: A dictionary mapping connector names to their corresponding classes.
+        """
+        connectors = {}
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj) and issubclass(obj, DatabaseConnector) and obj is not DatabaseConnector:
+                connectors[obj.__name__[:-9]] = obj  # Remove "Connector" from class name
+        return connectors
