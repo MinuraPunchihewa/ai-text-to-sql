cfe43c26747deecff3c2a559fa6c133e105d5dbf
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-22 00:39:42 +0530

    added the skeleton for the Bard LLM module

---
+++ ai_text_to_sql/llms/bard_llm.py
@@ -0,0 +1,18 @@
+import os
+import bardapi
+from typing import Text, Dict, List
+
+import logging
+import logging.config
+
+from ai_text_to_sql.llms.llm import LLM
+from ai_text_to_sql.config_parser import ConfigParser
+from ai_text_to_sql.connectors.database_connector import DatabaseConnector
+
+logging_config_parser = ConfigParser()
+logging.config.dictConfig(logging_config_parser.get_config_dict())
+logger = logging.getLogger()
+
+
+class BardLLM(LLM):
+    pass
