c8351b68c632cf0c35a531e50e587aa00767add3
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-21 13:19:18 +0530

    added LLMFactory for creating LLM implementations

---
+++ gpt_text_to_sql/llms/llm_factory.py
@@ -0,0 +1,37 @@
+import sys
+import inspect
+from typing import Optional, Dict, Type, Text
+
+from .llm import LLM
+from gpt_text_to_sql.llms import *
+
+
+class LLMFactory:
+    """
+    The class for building LLMs.
+    """
+    @staticmethod
+    def build_llm(name: Text, api_key: Text) -> LLM:
+        """
+        Build a LLM.
+        :param name: The name of the LLM.
+        :param api_key: The API key for the LLM.
+        :return: A LLM.
+        """
+        llms = LLMFactory._discover_llms()
+        if name in llms:
+            return llms[name](name, api_key)
+        else:
+            raise ValueError(f"Unsupported LLM: {name}")
+
+    @staticmethod
+    def _discover_llms() -> Dict[str, Type[LLM]]:
+        """
+        Discover available LLMs dynamically.
+        :return: A dictionary mapping LLM names to their corresponding classes.
+        """
+        llms = {}
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isclass(obj) and issubclass(obj, LLM) and obj is not LLM:
+                llms[obj.__name__[:-3]] = obj  # Remove "LLM" from class name
+        return llms
