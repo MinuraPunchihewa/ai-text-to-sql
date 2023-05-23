fa4a234112cdffe215225d0154963197ef0c4b9e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-23 13:40:06 +0530

    updated GPT to inherit from the new BaseAPILLM

--- ai_text_to_sql/llms/gpt_llm.py
+++ ai_text_to_sql/llms/gpt_llm.py
@@ -5,7 +5,7 @@ from typing import Text, Dict, List
 import logging
 import logging.config
 
-from ai_text_to_sql.llms.llm import LLM
+from ai_text_to_sql.llms.llm import BaseAPILLM
 from ai_text_to_sql.config_parser import ConfigParser
 from ai_text_to_sql.connectors.connector import Connector
 
@@ -14,7 +14,7 @@ logging.config.dictConfig(logging_config_parser.get_config_dict())
 logger = logging.getLogger()
 
 
-class GPT(LLM):
+class GPT(BaseAPILLM):
     """
     The class for interacting with the OpenAI API.
 
