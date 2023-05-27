49f008ec9eda470d8166ec579ef7f3e0d127c5d1
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-28 00:17:23 +0530

    updated the type hints for the LLM Factory

--- ai_text_to_sql/llms/llm_factory.py
+++ ai_text_to_sql/llms/llm_factory.py
@@ -2,7 +2,7 @@ import sys
 import inspect
 from typing import Optional, Dict, Type, Text
 
-from .llm import LLM
+from .llm import BaseLLM
 from ai_text_to_sql.llms import *
 
 
@@ -11,7 +11,7 @@ class LLMFactory:
     The class for building LLMs.
     """
     @staticmethod
-    def build_llm(name: Text, api_key: Text, **kwargs) -> LLM:
+    def build_llm(name: Text, api_key: Text, **kwargs) -> BaseLLM:
         """
         Build a LLM.
         :param name: The name of the LLM.
@@ -25,13 +25,13 @@ class LLMFactory:
             raise ValueError(f"Unsupported LLM: {name}")
 
     @staticmethod
-    def _discover_llms() -> Dict[str, Type[LLM]]:
+    def _discover_llms() -> Dict[str, Type[BaseLLM]]:
         """
         Discover available LLMs dynamically.
         :return: A dictionary mapping LLM names to their corresponding classes.
         """
         llms = {}
         for name, obj in inspect.getmembers(sys.modules[__name__]):
-            if inspect.isclass(obj) and issubclass(obj, LLM) and obj is not LLM:
+            if inspect.isclass(obj) and issubclass(obj, BaseLLM) and obj is not BaseLLM:
                 llms[obj.__name__] = obj
         return llms
