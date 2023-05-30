9a3edb1c6a02c9bddd1902d14fadf14516a6a79e
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-05-30 20:41:35 +0530

    added a simple unit test for MSSQL

--- tests/test_mssql_connector.py
+++ tests/test_mssql_connector.py
@@ -10,7 +10,21 @@ load_dotenv()
 class TestMSSQLConnector(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.tts = TextToSQL(
+            'MSSQL',
+            {
+                'user': os.environ.get("MSSQL_USER"),
+                'port': os.environ.get("MSSQL_PORT"),
+                'password': os.environ.get("MSSQL_PASSWORD"),
+                'host': os.environ.get("MSSQL_HOST"),
+                'database': os.environ.get("MSSQL_DATABASE"),
+                'schema': os.environ.get("MSSQL_SCHEMA")
+            },
+            api_key=os.environ.get("OPENAI_API_KEY")
+        )
 
     def test_convert_text_to_sql(self):
-        pass
+        self.assertEqual(
+            self.tts.convert_text_to_sql("Get me list of people whose last name is Torres."),
+            "SELECT * FROM person WHERE last_name = 'Torres'"
+        )
