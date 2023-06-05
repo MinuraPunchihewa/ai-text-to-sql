afe963ec83f69848896689759c86b10b3bb3dd0a
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-05 22:56:07 +0530

    updated the unit tests for SQLite

--- tests/test_sqlite_connector.py
+++ tests/test_sqlite_connector.py
@@ -26,23 +26,23 @@ class TestSQLiteConnector(unittest.TestCase):
 
     def test_advanced_convert_text_to_sql_1(self):
         self.assertEqual(
-            self.tts.convert_text_to_sql("Get me the names of 5 Rock songs.").replace('\n', ''),
+            self.tts.convert_text_to_sql("Get me the names of 5 Rock songs.").replace('\n', '').strip(),
             "SELECT Name \n"
             "FROM tracks \n"
             "INNER JOIN genres ON genres.GenreId = tracks.GenreId \n"
             "WHERE genres.Name = 'Rock' \n"
-            "LIMIT 5".replace('\n', '')
+            "LIMIT 5".replace('\n', '').strip()
         )
 
     def test_advanced_convert_text_to_sql_2(self):
         self.assertEqual(
-            self.tts.convert_text_to_sql("Find all the tracks written by AC/DC, including the track name, album title, and the artist name. Sort the results alphabetically by track name.").replace('\n', ' '),
+            self.tts.convert_text_to_sql("Find all the tracks written by AC/DC, including the track name, album title, and the artist name. Sort the results alphabetically by track name.").replace('\n', ' ').strip(),
             "SELECT t.Name AS Track, a.Title AS Album, ar.Name AS Artist \n"
             "FROM tracks t \n"
             "INNER JOIN albums a ON t.AlbumId = a.AlbumId \n"
             "INNER JOIN artists ar ON a.ArtistId = ar.ArtistId \n"
             "WHERE ar.Name = 'AC/DC' \n"
-            "ORDER BY t.Name ASC".replace('\n', '')
+            "ORDER BY t.Name ASC".replace('\n', '').strip()
         )
 
     def test_query(self):
