6d916ab9a1db9a9c002a4ad519cb3b933577c3b2
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-06-11 23:30:51 +0530

    added skeletons of all the abstract methods to the Bard Connector

--- ai_text_to_sql/llm_connectors/bard_connector.py
+++ ai_text_to_sql/llm_connectors/bard_connector.py
@@ -1,10 +1,13 @@
-from typing import Text
+from typing import Text, Dict
 
 from ai_text_to_sql.llm_connectors.llm_connector import LLMConnector
 
 
-class Bard(LLMConnector):
-    def create_prompt(self, user_input: Text, database_schema: Text) -> Text:
+class BardConnector(LLMConnector):
+    def format_database_schema(self, database_schema: Dict, connector_name: Text) -> Text:
+        pass
+
+    def create_prompt(self, user_input: Text, database_schema: Dict, connector_name: Text) -> Text:
         pass
 
     def get_answer(self, prompt: Text) -> Text:
