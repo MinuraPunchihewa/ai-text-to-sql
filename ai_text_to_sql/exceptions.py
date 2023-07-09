8d92ac4c0ea42b80e024919651f172840ac8741b
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-07-09 13:47:02 +0530

    added docstrings to the existing Exception classes

--- ai_text_to_sql/exceptions.py
+++ ai_text_to_sql/exceptions.py
@@ -1,19 +1,34 @@
 
 class ConnectionCreationException(Exception):
+    """
+    Raised when a connection cannot be created.
+    """
     pass
 
 
 class InsufficientParametersException(Exception):
+    """
+    Raised when insufficient parameters are provided to a connector for creating a connection.
+    """
     pass
 
 
 class NoMSSQLDriverException(Exception):
+    """
+    Raised when a MSSQL driver is not found for using the MSSQL connector.
+    """
     pass
 
 
 class UnsupportedDataConnectorException(Exception):
+    """
+    Raised when an attempt is made to use an unsupported data connector.
+    """
     pass
 
 
 class NoOpenAIAPIKeyException(Exception):
+    """
+    Raised when no OpenAI API key is provided for using the OpenAI connector.
+    """
     pass
