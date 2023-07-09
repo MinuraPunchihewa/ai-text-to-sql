4165697953baafe701cff84d53937ad30c2ded91
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-07-09 12:33:01 +0530

    updated the docstrings and method signatures of the MSSQLConnector to be more readable, and added the check for insufficient parameters

--- ai_text_to_sql/data_connectors/mssql_connector.py
+++ ai_text_to_sql/data_connectors/mssql_connector.py
@@ -1,12 +1,13 @@
 import pyodbc
-from typing import Text
+from typing import Text, Optional
 
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 
 from .data_connector import DataConnector
 
-from ai_text_to_sql.exceptions import ConnectionCreationException, NoMSSQLDriverException
+from ai_text_to_sql.exceptions import ConnectionCreationException, InsufficientParametersException,\
+    NoMSSQLDriverException
 
 
 class MSSQLConnector(DataConnector):
@@ -16,24 +17,37 @@ class MSSQLConnector(DataConnector):
     Parameters:
     -----------
     connection_string : Text
-        A SQLAlchemy connection string for the MSSQL database. This parameter is optional, but either this parameter or the user, password, host, port and database parameters must be specified.
+        A SQLAlchemy connection string for the MSSQL database. This parameter is optional, but either this parameter or
+        the user, password, host, port and database parameters must be specified.
     user : Text
-        The username to connect to the database. This parameter is optional, but either this parameter (in combination with the password, host, port and database parameters) or the connection_string parameter must be specified.
+        The username to connect to the database. This parameter is optional, but either this parameter (in combination
+        with the password, host, port and database parameters) or the connection_string parameter must be specified.
     password : Text
-        The password to connect to the database. This parameter is optional, but either this parameter (in combination with the user, host, port and database parameters) or the connection_string parameter must be specified.
+        The password to connect to the database. This parameter is optional, but either this parameter (in combination
+        with the user, host, port and database parameters) or the connection_string parameter must be specified.
     host : Text
-        The host name or IP address of the database server. This parameter is optional, but either this parameter (in combination with the user, password, port and database parameters) or the connection_string parameter must be specified.
+        The host name or IP address of the database server. This parameter is optional, but either this parameter (in
+        combination with the user, password, port and database parameters) or the connection_string parameter must be
+        specified.
     port : int
-        The port number of the database server. This parameter is optional, but either this parameter (in combination with the user, password, host and database parameters) or the connection_string parameter must be specified.
+        The port number of the database server. This parameter is optional, but either this parameter (in combination
+        with the user, password, host and database parameters) or the connection_string parameter must be specified.
     database : Text
-        The name of the database to connect to. This parameter is optional, but either this parameter (in combination with the user, password, host and port parameters) or the connection_string parameter must be specified.
+        The name of the database to connect to. This parameter is optional, but either this parameter (in combination
+        with the user, password, host and port parameters) or the connection_string parameter must be specified.
     schema : Text
-        The name of the schema to connect to. This parameter is optional. It can be provided as part of the connection_string parameter or as a separate parameter.
+        The name of the schema to connect to. This parameter is optional. It can be provided as part of the
+        connection_string parameter or as a separate parameter.
 
     """
     name = 'MSSQL'
 
-    def __init__(self, connection_string: Text = None, user: Text = None, password: Text = None, host: Text = None, port: Text = None, database: Text = None, schema: Text = None):
+    def __init__(self, connection_string: Optional[Text] = None, user: Optional[Text] = None,
+                 password: Optional[Text] = None, host: Optional[Text] = None, port: int = None,
+                 database: Optional[Text] = None, schema: Optional[Text] = None):
+        if not connection_string and not user and not password and not host and not port and not database:
+            raise InsufficientParametersException("Either the connection_string or the user, password, host, port and "
+                                                  "database parameters must be specified.")
         self.connection_string = connection_string
         self.user = user
         self.password = password
