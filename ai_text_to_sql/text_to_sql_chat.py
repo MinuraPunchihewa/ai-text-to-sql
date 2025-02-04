dde29ddc24d81b202b49e76b062484033e5f91ce
Minura Punchihewa <minurapunchihewa17@gmail.com>
2025-02-04 23:42:53 +0530

    added documentation to the new classes

--- ai_text_to_sql/text_to_sql_chat.py
+++ ai_text_to_sql/text_to_sql_chat.py
@@ -12,12 +12,30 @@ logger = logging.getLogger()
 
 
 class TextToSQLChatMemory:
+    """
+    The class for storing messages in memory.
+    The system prompt is not stored in memory, but is maintained separately.
+
+    Parameters:
+    -----------
+    system_prompt : Text
+        The system prompt to use.
+    window_size : Union[int, None]
+        The size of the window to use when retrieving messages from memory.
+        This parameter is optional, and defaults to None. If this parameter is not 
+        provided, all messages will returned.
+    """
     def __init__(self, system_prompt: Text, window_size: Union[int, None] = None):
         self.system_prompt = system_prompt
         self.window_size = window_size
         self.memory = []
 
     def add_message(self, role: Text, content: Text):
+        """
+        Add a message to the memory.
+        :param role: The author of the message.
+        :param content: The content of the message.
+        """
         self.memory.append(
             {
                 "role": role,
@@ -27,6 +45,13 @@ class TextToSQLChatMemory:
         )
 
     def get_messages(self):
+        """
+        Get the messages stored in memory.
+        If the window_size is not provided or the number of messages is less than the
+        window_size, all messages will be returned. The system prompt is always
+        included.
+        :return: The messages stored in memory.
+        """
         if self.window_size is None or len(self.memory) <= self.window_size:
             return [{"role": "system", "content": self.system_prompt}] + self.memory
         else:
@@ -38,6 +63,21 @@ class TextToSQLChatMemory:
 
 
 class TextToSQLChat(TextToSQL):
+    """
+    The class for converting text to SQL query and querying the database in a chat
+    setting.
+
+    Parameters:
+    -----------
+    llm_connector : LLMConnector
+        The LLMConnector to use for converting text to SQL query.
+    data_connector : DataConnector
+        The DataConnector to use for querying the database.
+    window_size : Union[int, None]
+        The size of the window to use when retrieving messages from memory.
+        This parameter is optional, and defaults to None. If this parameter is not 
+        provided, all messages will returned.
+    """
     def __init__(
             self,
             data_connector: DataConnector,
